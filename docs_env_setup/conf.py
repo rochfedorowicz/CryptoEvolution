2f1a770ce365813e254a10dea73bf348222f7313
Roch Fedorowicz <rochfed404@student.polsl.pl>
2026-02-16 00:12:11 +0100

    [DOC] Updating doc theme to RTD

--- docs_env_setup/conf.py
+++ docs_env_setup/conf.py
@@ -45,11 +45,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_theme_options = {
-    'page_width': '1400px',
-    'sidebar_width': '320px',
-    'body_max_width': 'none',
-    'fixed_sidebar': True,
-}
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
